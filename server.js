758401ee44cb4a015fdbcffa725d2d536e0add93
Ryutaro Maejima <taromakeitrighte@users.noreply.github.com>
2025-08-06 15:08:19 +0000

    commited from hahaha

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
